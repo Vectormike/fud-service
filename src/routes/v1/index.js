4e7d2309c936d6738ce8058fab619d6ee21a73a9
Victor Jonah <victorjonah199@gmail.com>
2020-09-19 12:59:02 +0100

    chore:

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
+const serviceRoute = require('./service.route');
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
+router.use('/services', serviceRoute);
 
 module.exports = router;

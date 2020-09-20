0634b8be5b373b7ab26dcd55c73a53647152ed83
Victor Jonah <victorjonah199@gmail.com>
2020-09-20 10:18:40 +0100

    feat: update, delete and query service

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -2,9 +2,28 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { serviceService } = require('../services');
 
+const getServices = catchAsync(async (req, res) => {
+  const services = await serviceService.getServices();
+  res.status(httpStatus.OK).json({ services });
+});
+
+const getService = catchAsync(async (req, res) => {
+  const service = await serviceService.getService(req.params.serviceId);
+  res.status(httpStatus.OK).json({ service });
+});
+
 const createService = catchAsync(async (req, res) => {
   const service = await serviceService.createService(req.body);
   res.status(httpStatus.CREATED).json({ service });
 });
 
-module.exports = { createService };
+const updateService = catchAsync(async (req, res) => {
+  const service = await serviceService.updateService(req.params.serviceId, req.body);
+  res.status(httpStatus.CREATED).json({ service });
+});
+
+const deleteService = catchAsync(async (req, res) => {
+  await serviceService.deleteService(req.params.serviceId);
+  res.status(httpStatus.OK).end();
+});
+module.exports = { getServices, getService, createService, updateService, deleteService };
